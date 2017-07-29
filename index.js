d123a4afa731a125f62e3743ec180d2dcf062d2a
amnotafraid <amnotafraid@gmail.com>
2017-07-28 21:08:53 -0500

    try at disconnect

--- index.js
+++ index.js
@@ -19,15 +19,39 @@ var clients = [];
 io.on('connection', (socket) => {
 	console.log('made socket connection, id = ', socket.id);
 
+  socket.on('disconnect', (socket) => {
+    // TODO figure out how to delete clients when they are closed
+    for (let i = 0; i < clients.length; i++) {
+      let bRemove = false;
+      if (clients[i].hasOwnProperty('phoneSocketId') &&
+          clients[i].phoneSocketId == socket.id) { // phone disconnected
+        bRemove = true;
+        if (clients[i].hasOwnProperty('mirrorSocketId')) {
+          socket.broadcast.to(clients[i].mirrorSocketId).emit('problem', 'phone disconnected');
+        }
+      }
+      if (clients[i].hasOwnProperty('mirrorSocketId') &&
+          clients[i].mirrorSocketId == socket.id) { // mirror disconnected
+        bRemove = true;
+        if (clients[i].hasOwnProperty('phoneSocketId')) {
+          socket.broadcast.to(clients[i].phoneSocketId).emit('problem', 'mirror disconnected');
+        }
+      }
+      if (bRemove) {
+        clients.splice(i, 1);
+      }
+    } 
+    console.log('clients = ' + JSON.stringify(clients, null, 2));
+  });
 	socket.on('mirror', (code) => {
     let bUpdated = false;
     for (let i = 0; i < clients.length; i++) {
-      if (clients[i].mirrorSocketId === socket.id) {
+      if (clients[i].mirrorSocketId == socket.id) {
         clients[i].code = code;
         bUpdated = true;
         if (clients[i].hasOwnProperty('phoneSocketId')) {
           socket.broadcast.to(clients[i].phoneSocketId).emit('problem', 'mirror has new code');
-          delete clients[i].mirrorSocketId;
+          delete clients[i].phoneSocketId;
         }
         break;
       }
@@ -47,12 +71,12 @@ io.on('connection', (socket) => {
         mirrorSocketId = clients[i].mirrorSocketId;
         if (clients[i].hasOwnProperty('phoneSocketId') &&
             clients[i].phoneSocketId !== socket.id) {
-          socket.broadcast.to(clients[i].phoneSocketId).emit('problem', 'different phone connected');
+          socket.broadcast.to(clients[i].phoneSocketId).emit('problem', '');
         }
         clients[i].phoneSocketId = socket.id;
       }
     }
-    if (mirrorSocketId === '') {
+    if (mirrorSocketId == '') {
       socket.emit('problem', 'no mirror found for that code');
     }
     else {
