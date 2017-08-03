2bb996baf81dd362081b3e523e7bbdc308ec5a8c
amnotafraid <amnotafraid@gmail.com>
2017-08-03 11:30:04 -0500

    code is 4 digits. another app cannot connect. mirror button message toggle

--- public/mirror.js
+++ public/mirror.js
@@ -7,17 +7,25 @@ var btn = document.getElementById('send'),
     feedback = document.getElementById('feedback');
 
 var code;
-var phoneSocket;
+var phoneSocket = null;
+
+console.log ('btn.innerText = ' + btn.innerText);
+if (phoneSocket === null) {
+  btn.innerText = 'Get a code for your phone';
+  console.log('phoneSocket = null');
+  console.log ('btn.innerText = ' + btn.innerText);
+}
 
 // emit events
 btn.addEventListener('click', () => {
-  code = Math.floor(100000 + Math.random() * 900000);
+  code = Math.floor(1000 + Math.random() * 9000);
   output.innerHTML = '<p>Scan</p>'
                     +'<img src="qr.png">'
                     +'<p>or go to http://localhost:4000/phone.html</br>'
                     +'and enter <strong>' + code + '</strong></p>';
   feedback.innerHTML = '<p></p>';
   phoneSocket = null;
+  btn.innerText = 'Get a code for your phone';
   socket.emit('mirror', code);
 });
 
@@ -26,9 +34,11 @@ socket.on('phoneConnected', (id) => {
   output.innerHTML = '';
   feedback.innerHTML = '<p><em>Your phone is now connected</em></p>';
   phoneSocket = id;
+  btn.innerText = 'Reset Phone Code';
 });
 
 socket.on('problem', (message) => {
   feedback.innerHTML = '<p class="problem"><em>' + message + '</em></p>';
   phoneSocket = null;
+  btn.innerText = 'Get a code for your phone';
 });
